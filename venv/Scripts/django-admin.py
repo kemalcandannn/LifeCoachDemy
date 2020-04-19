10def7beb31f59341a5a59bf9dea21aee7823cff
kemalcandannn <kemalcandan0735@gmail.com>
2020-04-20 02:23:13 +0300

    First Commit

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\Kemal CANDAN\Desktop\Project\BBM471 Project\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
